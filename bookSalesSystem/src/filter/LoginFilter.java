6be1f29972f74016e6940e3a313fe915dde21415
510850111 <510850111@qq.com>
2017-06-27 10:35:57 +0800

    添加了一些必要的jar包,新增登陆过滤.

---
+++ bookSalesSystem/src/filter/LoginFilter.java
@@ -0,0 +1,34 @@
+package filter;
+
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+@WebFilter(filterName = "LoginFilter",urlPatterns = {"login.jsp"})
+public class LoginFilter implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        HttpSession session = request.getSession(); // 取得session接口对象
+        if (session.getAttribute("aid") != null){ // 表示有内容存在
+            filterChain.doFilter(servletRequest,servletResponse);
+        }else{
+            request.setAttribute("msg","你还未登录，请先登录后操作!");
+            request.setAttribute("url","/login.jsp");
+            request.getRequestDispatcher("/pages/forward.jsp").forward(servletRequest,servletResponse);
+        }
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
+
