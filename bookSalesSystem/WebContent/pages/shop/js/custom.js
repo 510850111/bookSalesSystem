337c90ae9607d2626a327b6397fb0d135d6b07da
510850111 <510850111@qq.com>
2017-07-11 17:16:41 +0800

    新建书籍的动态获取

--- bookSalesSystem/WebContent/pages/shop/js/custom.js
+++ bookSalesSystem/WebContent/pages/shop/js/custom.js
@@ -1,10 +1,17 @@
+var catelogry = document.getElementById("isol").getElementsByTagName("ul")[0];
 function getCatelogry() {
-	$.ajax({
+	var catelogryHTMLCode = "";
+	$
+			.ajax({
 				type : "GET",
 				url : "http://localhost:8080/bookSalesSystem/pages/BookServlet/getCategory",
 				success : function(data) {
-					console.log("success" + data);
-					alert("data=" + data);
+					data = eval(data);
+					for (var i = 0; i < data.length; i++) {
+						catelogryHTMLCode += "<li><a href='#'>" + data[i].categoryName+ "</a></li>";
+					}
+					catelogry.innerHTML = catelogryHTMLCode;
+
 				},
 				error : function(e) {
 					console.log("failed" + JSON.stringify(e));
@@ -13,13 +20,24 @@ function getCatelogry() {
 }
 
 function getAllBooks() {
+	var bookList = document.getElementById("urunliste");
+	var bookListHTMLCode = "";
 	$.ajax({
-
 				type : "GET",
 				url : "http://localhost:8080/bookSalesSystem/pages/BookServlet/getAllBooks",
 				success : function(data) {
-					console.log("[getAllBooks]data=" + JSON.stringify(data));
-					alert("[getAllBooks]data=" + JSON.stringify(data));
+					data = eval(data);
+					for(var i = 0 ; i< data.length ; i++){
+						bookListHTMLCode += "<div class='urunliste'><div class='urun'>";
+						bookListHTMLCode += "<a href='#' class = 'uResim'><img src='pages/shop/img/uresim.png' alt=''/></a>";
+						bookListHTMLCode += "<a href='#' class='uBaslik'>" + data[i].bookName + "</a>";
+						bookListHTMLCode += "<a href='Details.html' class='uAbaslik'>" + data[i].categoryName + "</a>";
+						bookListHTMLCode += "<p>" + data[i]._abstract + "</p>";
+						bookListHTMLCode += "<span>" +data[i].price+ "￥</span>";
+						bookListHTMLCode += "<a href='#' class='incele'></a><a href='#' class='sepeteat'></a>";
+						bookListHTMLCode += "</div></div></div>"
+					}
+					bookList.innerHTML = bookListHTMLCode;
 				},
 				error : function(e) {
 					console.log("[getAllBooks]failed" + JSON.stringify(e));
